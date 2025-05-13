af0a721c7c8f1f9b1ff3f210bd803700b5f89c69
Loay Alrefai <121945616+Loay-99@users.noreply.github.com>
2025-05-13 18:39:16 +0200

    Create WorkerInterface.java

---
+++ WorkerInterface.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface WorkerInterface extends Remote {
+    Color[][] bild_rechnen_worker(boolean show_layer_line, float farbe_number, int workers_threads, int max_iter, double max_betrag, int y_sta, int y_sto, int xpix, int ypix, double xmin, double xmax, double ymin, double ymax) throws RemoteException;
+    boolean worker_status() throws RemoteException;
+    void worker_buchen() throws RemoteException;
+}
